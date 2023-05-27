06f8be03a0f31e86216d2bfbc6f0b37d18843484
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2023-05-27 13:11:29 +0500

    Update transContext.js

--- react-app-1/src/transContext.js
+++ react-app-1/src/transContext.js
@@ -2,10 +2,7 @@ import React, { createContext, useReducer } from "react";
 import TransactionReducer from "./transReducer";
 
 const initialTransactions = [
-    { amount: +500, desc: "Salary" },
-    { amount: -40, desc: "Book" },
-    { amount: -200, desc: "Package" },
-    { amount: -100, desc: "Drink" }
+    
 ]
 
 export const TransactionContext = createContext(initialTransactions);
